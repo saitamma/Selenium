fb6860eb4fc56cc98e5e3c80db7701a7cf91576e
sailakshmi <saitamma@cisco.com>
2015-10-24 15:31:01 +0530

    Pushing selenium project

---
+++ SampleSelenium/src/jenkins/NewTest.java
@@ -0,0 +1,35 @@
+package jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+  @Test
+   public void sampletest()
+  {
+	  WebDriver driver=new FirefoxDriver();
+	  driver.get("http://www.google.com");
+	  System.out.println(driver.getTitle());
+	  driver.manage().window().maximize();
+  }
+  @Test
+  public void sampletest1()
+ {
+	  WebDriver driver=new FirefoxDriver();
+	  driver.get("http://www.gmail.com");
+	  System.out.println(driver.getTitle());
+	  driver.manage().window().maximize();
+ }
+  @Test
+  public void sampletest2()
+ {
+	  WebDriver driver=new FirefoxDriver();
+	  driver.get("http://www.facebook.com");
+	  System.out.println(driver.getTitle());
+	  driver.manage().window().maximize();
+ }
+
+
+}
