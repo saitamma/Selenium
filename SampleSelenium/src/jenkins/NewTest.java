de4475a2cdbc3ff0b4bc9e66ae8c2b8bcae9c460
sailakshmi <saitamma@cisco.com>
2015-10-28 16:23:49 +0530

    hi...

--- SampleSelenium/src/jenkins/NewTest.java
+++ SampleSelenium/src/jenkins/NewTest.java
@@ -26,7 +26,7 @@ public class NewTest {
   public void sampletest2()
  {
 	  WebDriver driver=new FirefoxDriver();
-	  driver.get("http://www.google.com");
+	  driver.get("http://www.facebook.com");
 	  System.out.println(driver.getTitle());
 	  driver.manage().window().maximize();
  }
